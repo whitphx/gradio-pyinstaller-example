b4f54d136ba7a1f38851c2e3baeba177df8c4b0b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-11-27 22:17:50 +0900

    Use demo.local_url

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import webview
 
 logger = logging.getLogger(__name__)
 
+WINDOW_TITLE = "Gradio App"
+
+logger.info("Starting Gradio app")
 from gradio_app import demo
 demo.launch(prevent_thread_lock=True)
 
-gradio_url = "http://localhost:7860/"
-
 logger.info("Starting webview")
-webview.create_window('Hello world', gradio_url)
+webview.create_window(WINDOW_TITLE, demo.local_url)
 webview.start()
-
-logger.info("Terminating Gradio app")
