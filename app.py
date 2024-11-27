b9a6d64e0c78e0e43f126df81af490dc2825a934
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-11-27 20:40:16 +0900

    PyWebView app (#1)
    * PyWebView app
    
    * Improve PyInstaller setup

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-import gradio as gr
+import logging
+import time
+
+import webview
+
+logger = logging.getLogger(__name__)
 
 
-def greet(name):
-    return "Hello " + name + "!!"
+def wait_for_url(url: str, max_retries: int = 30, retry_interval: int = 1):
+    import urllib.request
+    import urllib.error
+
+    for _ in range(max_retries):
+        try:
+            urllib.request.urlopen(url)
+            break
+        except urllib.error.URLError:
+            time.sleep(retry_interval)
+    else:
+        raise Exception(f"Failed to connect to {url} after {max_retries * retry_interval} seconds")
 
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+from gradio_app import demo
+demo.launch(prevent_thread_lock=True)
 
-if __name__ == "__main__":
-    demo.launch()
+gradio_url = "http://localhost:7860/"
+
+wait_for_url(gradio_url)
+
+logger.info("Starting webview")
+webview.create_window('Hello world', gradio_url)
+webview.start()
+
+logger.info("Terminating Gradio app")
