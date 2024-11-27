a8cad3a2ba6345028667e00739f6f2f829331a55
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-11-27 22:21:17 +0900

    Simplify app.py

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-import logging
-
 import webview
 
-logger = logging.getLogger(__name__)
-
-WINDOW_TITLE = "Gradio App"
-
-logger.info("Starting Gradio app")
 from gradio_app import demo
+
+
 demo.launch(prevent_thread_lock=True)
 
-logger.info("Starting webview")
-webview.create_window(WINDOW_TITLE, demo.local_url)
+webview.create_window("Gradio App", demo.local_url)
 webview.start()
